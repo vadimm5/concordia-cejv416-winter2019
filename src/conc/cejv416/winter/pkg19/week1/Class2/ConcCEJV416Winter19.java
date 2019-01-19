9c53f28b0a45bb190e5f81330a7fa7df2d77c905
V_ALINOS <V_ALINOS@CCE-FB107-11W.concordia.ca>
2019-01-18 19:43:43 -0500

    Added a print statement.

--- src/conc/cejv416/winter/pkg19/week1/Class2/ConcCEJV416Winter19.java
+++ src/conc/cejv416/winter/pkg19/week1/Class2/ConcCEJV416Winter19.java
@@ -16,6 +16,8 @@ public class ConcCEJV416Winter19 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("This is my first github project");
     }
     
 }
