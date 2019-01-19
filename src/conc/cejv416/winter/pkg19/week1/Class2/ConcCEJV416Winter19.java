f791dc8aac0f3d10a16b687bd95a97dc4b254874
V_ALINOS <V_ALINOS@CCE-FB107-11W.concordia.ca>
2019-01-18 19:33:35 -0500

    Initial commit

---
+++ src/conc/cejv416/winter/pkg19/week1/Class2/ConcCEJV416Winter19.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conc.cejv416.winter.pkg19.week1.Class2;
+
+/**
+ *
+ * @author V_ALINOS
+ */
+public class ConcCEJV416Winter19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
