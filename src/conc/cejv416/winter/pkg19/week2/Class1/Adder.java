bed5f71aebeaaa4106fa6861a4e2c482b9e12971
V_ALINOS <V_ALINOS@CCE-FB107-11W.concordia.ca>
2019-01-23 21:22:32 -0500

    test 3

---
+++ src/conc/cejv416/winter/pkg19/week2/Class1/Adder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conc.cejv416.winter.pkg19.week2.Class1;
+
+/**
+ *
+ * @author V_ALINOS
+ */
+public class Adder {
+    
+    int num1 = 0;
+    int num2 = 0;
+    
+    public void add()
+    {
+        System.out.println("The value method are " + (num1 + num2));
+    }
+    
+    
+}
